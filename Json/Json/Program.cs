da3d75097264d1b2a2300c57e92f978f90be582e
tahmidh <tahmidh@softpauer.com>
2022-06-10 13:46:08 +0100

    first commit

--- Json/Json/Program.cs
+++ Json/Json/Program.cs
@@ -1,56 +1,51 @@
-﻿using Newtonsoft.Json;
+﻿
 using System;
 using System.Collections.Generic;
+using System.Text.Json;
 
-public class Account
+
+namespace DeserializeExtra
 {
-    public string Email { get; set; }
-    public bool Active { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public IList<string> Roles { get; set; }
+    public class Rootobject
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string[] Roles { get; set; }
+    }
 
-}
-class Program
-    //serialise:convert an obj to json
-{
-    static void Main(string[] args)
 
-    {// create an obj and add data to the properties
-        Account account = new Account()
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            Email = "james@example.com",
-            Active = true,
-            CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
-            Roles = new List<string>
-                    {
-                        "User",
-                        "Admin"
-                    }
-        };
-         // from the Newtonsoft.Json package get JsonConvert that access serialisedObj()
-         //thisfunc take 2args the obj beinf sealised and the format for the json output
-        string json = JsonConvert.SerializeObject(account, Formatting.Indented);
-        Console.WriteLine(json);
 
-        //desealise: convert json to obj
-                string json2 = @"{
-          'Email': 'james@example.com',
-          'Active': true,
-          'CreatedDate': '2013-01-20T00:00:00Z',
-          'Roles': [
-            'User',
-            'Admin'
-          ]
-        }";
-        Account account2 = JsonConvert.DeserializeObject<Account>(json2);
-        Console.WriteLine(account2.Email);
+                        string json = @"{
+              'Email': 'james@example.com',
+              'Active': true,
+              'CreatedDate': '2013-01-20T00:00:00Z',
+              'Roles': [
+                'User',
+                'Admin'
+              ]
+            }";
 
 
+            Rootobject obj = JsonSerializer.Deserialize<Rootobject>(json);
+            Console.WriteLine(obj.Email);   
 
-
-
-
+        }
+        
 
 
     }
+
+
+
+
+
+
+
+
+
 }
--- my_csharp/my_csharp/Program.cs
+++ my_csharp/my_csharp/Program.cs
@@ -1,110 +1,101 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Practise_csharp{
+namespace Reactdata {
 
-    public class Product_details
-    { //properties
-    public string Caterory { get; set; }
-    public string Price { get; set; }
+    class MyClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
 
-    public bool Stocked { get; set; }
-    public string Name { get; set; }
-        
-        public void my_method()
-        {
-            Console.WriteLine($"My products category =  {Caterory}" +
-                $"product price={Price}" +
-                $"product stocked={Stocked}" +
-                $"product name{Name}");
-        }                    
-        
 
     }
-    public class MyClass { 
-        public int Id { get; set; }    
-        public  string Subject { get; set; }
-        public String Name { get; set; }
-    
-    }   
-
-    class Program
+    class Product_details
     {
-        static void Main(string[] args)
 
-         {
-            var dict = new Dictionary<string, List<Product_details>>();
+        public string Category { get; set; }
+        public string Price { get; set; }
+        public bool Stocked { get; set; } 
+        public string Name { get; set; }
 
-
-            //product_detail class obj
-           // Product_details product_obj=new Product_details();
-           //my list name Product_list
-            List<Product_details> Product_list = new List<Product_details>() {
-                new Product_details{Caterory= "Fruits", Price= "$1", Stocked= true, Name= "Apple" },
-                new Product_details{Caterory= "Fruits", Price= "$1", Stocked= true, Name= "Dragonfruit" },
-                new Product_details{Caterory= "Fruits", Price= "$2", Stocked= false, Name= "Passionfruit" },
-                new Product_details{Caterory= "Vegetables", Price= "$2", Stocked= true, Name= "Spinach" },
-                new Product_details{Caterory= "Vegetables", Price= "$4", Stocked= false, Name= "Pumpkin" },
-                new Product_details{Caterory= "Vegetables", Price= "$1", Stocked= true, Name= "Peas" },
-             };
-            for (var item = 0; item < Product_list.Count; item++)
-            {
-               /* Console.WriteLine($"category :  {Product_list[item].Caterory } " +
-               $"product price : {Product_list[item].Price} " +
-               $"product stocked : {Product_list[item].Stocked} " +
-               $"product name : {Product_list[item].Name} ");*/
-            }
-            Console.WriteLine(" ");
-            //
-            var category_names = Product_list.GroupBy(x => x.Caterory);//.Select(user => user.Key);
-            foreach (var name in category_names)
-            { 
-                Console.WriteLine(name.Key);
-                foreach (var item in name)
-                {
-                    Console.WriteLine($"{item.Name}  {item.Price} {item.Stocked}");
-
-                }
-                Console.WriteLine(" ");
+    }
 
 
-            }
-            MyClass obj=new MyClass();//not necessary
+    class Program {
+
+
+        static void Main(string[] args) {
             List<MyClass> mylist = new List<MyClass> {
            new MyClass(){Id = 208, Subject = "some text",Name= "Apple" },
            new MyClass(){Id = 209, Subject = "another text",Name= "juice" },
            new MyClass(){Id = 210, Subject = "one text",Name= "orange" },
            new MyClass(){Id = 211, Subject = "text mix",Name= "lemon" },
-          // new MyClass(){Id = 211, Subject = "text mix",Name= "Apple" }
+           new MyClass(){Id = 211, Subject = "text mix",Name= "Apple" }
             };
 
-            Dictionary<int, MyClass> dicts = mylist.ToDictionary(obj => obj.Id,obj=>obj);
-            foreach ((var k,var v) in dicts)
+            //
+            mylist.Add(new MyClass() { Id = 212, Subject = "text mix", Name = "Apple" });
+            mylist.Add(new MyClass() { Id = 208, Subject = "text mix", Name = "Apple" });
+
+
+            List<Product_details> Product_list = new List<Product_details>() {
+                new Product_details{Category= "Fruits", Price= "$1", Stocked= true, Name= "Apple" },
+                new Product_details{Category= "Fruits", Price= "$1", Stocked= true, Name= "Dragonfruit" },
+                new Product_details{Category= "Fruits", Price= "$2", Stocked= false, Name= "Passionfruit" },
+                new Product_details{Category= "Vegetables", Price= "$2", Stocked= true, Name= "Spinach" },
+                new Product_details{Category= "Vegetables", Price= "$4", Stocked= false, Name= "Pumpkin" },
+                new Product_details{Category= "Vegetables", Price= "$1", Stocked= true, Name= "Peas" },
+            };
+            Product_list.Add(new Product_details
             {
-                Console.WriteLine(k);
-                Console.WriteLine($"{v.Subject} {v.Name} ");
+                Category = "Meat",
+                Price = "$1",
+                Stocked = true,
+                Name = "Chicken"
+            });
+
+            foreach (MyClass item in mylist) {
+               // Console.WriteLine($"{item.Id}");
+               //Console.WriteLine($" {item.Name} {item.Subject}");
+
+
             }
 
+            //Dictionary<string,List<MyClass>> mydict = new Dictionary<string,List<MyClass>>();
 
+            var mydict = mylist.GroupBy(x => x.Id);// ToDictionary(x => x.Key, x => x);
+            var mydict2 = mydict.ToDictionary(x=>x.Key);
 
-            //var res = Product_list.Distinct().ToDictionary(x => x.Caterory,x=>x.Caterory);
-            /*foreach ((var k,var v) in res)
+            foreach (var item in mydict2)
             {
-                Console.WriteLine($" Category :{k}{v.SelectMany()} ");//{res.Values}
-                
-                //Console.WriteLine($"{v} {v.Price} {v.Stocked}");
+                Console.WriteLine(item.Key);
+
+                foreach (var item2 in item.Value)
+                {
+                    Console.WriteLine($"{item2.Subject} {item2.Name}");
+                  
+                } Console.WriteLine(" ");
+
+            }
+            var groupBy_category = Product_list.GroupBy(x => x.Category);// ToDictionary(x => x.Key, x => x);
+            var Product_dict = groupBy_category.ToDictionary(x => x.Key);
+
+            foreach (var item in Product_dict)
+            {
+                Console.WriteLine($"Categories {item.Key}");
+
+                foreach (var item2 in item.Value)
+                {
+                    Console.WriteLine($"{item2.Name} {item2.Price}");
+
+                }
                 Console.WriteLine(" ");
 
-            }*/
-
-
-
-
-
+            }
 
 
 
@@ -116,5 +107,10 @@ namespace Practise_csharp{
 
         }
 
-    }
+
+
+        }
+
+
 }
+
